bf45a191ae1560ad093ee1b509529751d23b0e5e
gelissa <gelissaleveille@yahoo.com>
2019-10-30 19:20:02 -0400

    Updates

---
+++ precache-manifest.cd0b0878a77beac00b66d15c980a290b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fd2d9e9c5946ee8cd14f29dd0912cc",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e7100ff4f9ce7be7586b",
+    "url": "/clickyGame/static/css/main.be4bb160.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/clickyGame/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "e7100ff4f9ce7be7586b",
+    "url": "/clickyGame/static/js/main.f208388c.chunk.js"
+  },
+  {
+    "revision": "08e2405d66dd095e56b5",
+    "url": "/clickyGame/static/js/runtime-main.250ea920.js"
+  }
+]);
